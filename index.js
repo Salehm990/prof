ccb516ea183e1f6cb5544489a643dc6f310918cf
unknown <salehmohsen.oip.99@gmail.com>
2022-02-10 12:19:43 +0200

    3rd

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ fetch('https://apidatta.herokuapp.com/Data')
 .then(json => {
     var SM = document.getElementById('SM');
     var OriginalContent = `<div>
-          <div class="icon"><i class="MANGALA"></i></div>
-          <h4 class="title"><a href="">FADY</a></h4>
-          <p class="description">CHRIST</p>
+          <div class="icon"><i class="F1"></i></div>
+          <h4 class="title"><a href="">F2</a></h4>
+          <p class="description">F3</p>
         </div>`;
     for(var i=0;i<json.length;i++)
     {
         var MyContent = document.createElement('div');
         var CustomisedContent = OriginalContent;
-        CustomisedContent = CustomisedContent.replace('FADY',json[i].title);
-        CustomisedContent = CustomisedContent.replace('CHRIST',json[i].description);
-        CustomisedContent = CustomisedContent.replace('MANGALA',json[i].icon);
+        CustomisedContent = CustomisedContent.replace('F2',json[i].title);
+        CustomisedContent = CustomisedContent.replace('F3',json[i].description);
+        CustomisedContent = CustomisedContent.replace('F1',json[i].icon);
         MyContent.className='col-lg-4 col-md-6 icon-box'
         MyContent.innerHTML = CustomisedContent;
         SM.appendChild(MyContent);
